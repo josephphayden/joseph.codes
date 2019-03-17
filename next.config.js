dfdbe0c58eb757879ef220612e095d1ef1cddaad
Joseph Hayden <josephphayden@gmail.com>
2019-03-17 15:59:11 +0000

    Update to produce static export from Next

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
-const withSass = require("@zeit/next-sass");
+const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({ cssModules: true });
+module.exports = withSass({
+    cssModules: true,
+    exportPathMap: () => ({
+        '/': { page: '/' },
+        '/background': { page: '/background' },
+        '/services': { page: '/services' },
+        '/contact': { page: '/contact' }
+    })
+});
