3adb45bff16813891e207f7229b48afe50f3fa32
Joseph Hayden <josephphayden@gmail.com>
2019-03-17 19:08:47 +0000

    Add favicons

--- components/header/Header.js
+++ components/header/Header.js
@@ -5,6 +5,22 @@ const Header = ({ title }) => (
     <>
         <Head>
             <title>{`${title} | Joseph Hayden`}</title>
+            <link rel='icon' href='/static/favicons/favicon.ico' />
+            <link
+                rel='icon'
+                href='/static/favicons/favicon-16x16'
+                sizes='16x16'
+            />
+            <link
+                rel='icon'
+                href='/static/favicons/favicon-32x32'
+                sizes='32x32'
+            />
+            <link
+                rel='apple-touch-icon'
+                href='/static/favicons/apple-touch-icon.png'
+            />
+            <link rel='manifest' href='/static/manifest.json' />
             <meta
                 name='viewport'
                 content='initial-scale=1.0, width=device-width'
